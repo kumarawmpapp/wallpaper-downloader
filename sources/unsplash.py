6d5dd3ebec07316e3df1cd4d376277e0aa4a6bb3
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-08-20 20:53:08 +0530

    add topics for unsplash

--- sources/unsplash.py
+++ sources/unsplash.py
@@ -4,7 +4,7 @@ import os
 
 CONFIG = {
     "endpoint": "https://api.unsplash.com/photos/random",
-    "params": {"query": "{query}", "count": "{count}", "orientation": "landscape", "content_filter": "high"},
+    "params": {"query": "{query}", "count": "{count}", "orientation": "landscape", "content_filter": "high", "topics": "bo8jQKTaE0Y,6sMVjTLSkeQ,M8jVbLbTRws,tthdwfNPCcw,"},
     "response_map": "[].urls.full",
 }
 
@@ -20,3 +20,6 @@ def get_image_urls(query, count, config_override=None):
 
     response = requests.get(config["endpoint"], params=params).json()
     return jmespath.search(config["response_map"], response)
+
+
+# https://api.unsplash.com/topics?per_page=100&order_by=featured&client_id=MBR8UbWLuZqGsSqvSlMmepScIXql7DMQvhuv2J8o6Io
